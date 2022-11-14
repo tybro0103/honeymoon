6b4a11f2169f6a43481de0266cb5322360523fff
Tyler Brown <tyler@tybro.io>
2022-11-14 10:03:19 -0500

    tweaks

--- build.js
+++ build.js
@@ -10,24 +10,11 @@ console.log('----tybroData.ssImageUrl', tybroData.ssImageUrl);
 const baseHtmlPath = path.join(__dirname, '/docs/base.html');
 const baseHtml = fs.readFileSync(baseHtmlPath, 'utf8');
 
-const numOfImages = 66;
- 
-// _.range(1, numOfImages+1).forEach((imgNum) => {
-//   const imgHtmlPath = path.join(__dirname, `/docs/${imgNum}.html`);
-//   const imgHtml = baseHtml.replace('<!-- DYNAMIC_HEAD -->', `
-//     <title>#${imgNum} | ❤️ Tyler & Marisa Brown</title>
-//     <meta property="og:title" content="#${imgNum} | ❤️ Tyler & Marisa Brown" />
-//     <meta property="og:image" content="/images/large/DSCF0426-edit-orig.jpeg" />
-//   `);
-//   fs.writeFileSync(imgHtmlPath, imgHtml);
-//   console.log('---- imgHtmlPath', imgHtmlPath);
-// });
-
 tybroData.images.forEach((img, index) => {
   const imgHtmlPath = path.join(__dirname, `/docs/${img.key}.html`);
   const imgHtml = baseHtml.replace('<!-- DYNAMIC_HEAD -->', `
-    <title>#${img.key} | ❤️ Tyler & Marisa Brown</title>
-    <meta property="og:title" content="#${img.key} | ❤️ Tyler & Marisa Brown" />
+    <title>#${img.key} | Tyler & Marisa Brown ❤️</title>
+    <meta property="og:title" content="#${img.key} | Tyler & Marisa Brown ❤️" />
     <meta property="og:image" content="${img.urlLarge}" />
   `);
   fs.writeFileSync(imgHtmlPath, imgHtml);
@@ -36,8 +23,8 @@ tybroData.images.forEach((img, index) => {
 
 const indexHtmlPath = path.join(__dirname, `/docs/index.html`);
 const indexHtml = baseHtml.replace('<!-- DYNAMIC_HEAD -->', `
-  <title>❤️ Tyler & Marisa Brown</title>
-  <meta property="og:title" content="❤️ Tyler & Marisa Brown" />
+  <title>Tyler & Marisa Brown ❤️</title>
+  <meta property="og:title" content="Tyler & Marisa Brown ❤️" />
   <meta property="og:image" content="${tybroData.ssImageUrl}" />
 `);
 fs.writeFileSync(indexHtmlPath, indexHtml);
